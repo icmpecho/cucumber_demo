fdb9243a248ef4d7aa1a12a3b5865c387f4a2609
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2014-07-25 00:32:51 +0700

    Add chrome support

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,4 +2,8 @@ require 'selenium-webdriver'
 require 'capybara/cucumber'
 require 'rspec/expectations'
 
-Capybara.default_driver = :selenium
+Capybara.default_driver = :selenium_chrome
+
+Capybara.register_driver :selenium_chrome do |app|
+  Capybara::Selenium::Driver.new( app, :browser => :chrome )
+end
