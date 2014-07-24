f32e63856f032bb0ca7cc09c94aa93235c5ac689
Pinnapong Silpsakulsuk <pinnapong@gmail.com>
2014-07-25 00:38:28 +0700

    Add safari support

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,8 +2,12 @@ require 'selenium-webdriver'
 require 'capybara/cucumber'
 require 'rspec/expectations'
 
-Capybara.default_driver = :selenium_chrome
+Capybara.default_driver = :selenium_safari
 
 Capybara.register_driver :selenium_chrome do |app|
   Capybara::Selenium::Driver.new( app, :browser => :chrome )
+end
+
+Capybara.register_driver :selenium_safari do |app|
+  Capybara::Selenium::Driver.new( app, :browser => :safari )
 end
